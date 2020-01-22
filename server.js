64e4a174fc832dcc29bde04eb6cc117ce2ed8b73
Daron Short <daron.short@hotmail.com>
2020-01-21 21:32:05 -0600

    Added Handler for promise errors

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDb = require("./config/db");
 
 //Config
 dotenv.config({ path: "./config/config.env" });
+
+//Connect Db
+connectDb();
 
 //Routes
 const bootcamps = require("./routes/bootcamps");
@@ -11,4 +15,11 @@ const app = express();
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log("Server is running"));
+const server = app.listen(PORT, console.log("Server is running"));
+
+process.on("unhandleRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
