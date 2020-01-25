849c4a014d9d426d5ecf9c0c30aee54ef15563af
Daron Short <daron.short@hotmail.com>
2020-01-24 20:06:23 -0600

    Updated Controller methods to use custom errorhandler

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const Bootcamp = require("../models/Bootcamp");
+const ErrorResponse = require("../util/errorResponse");
 
 //Get All Bootcamps
 exports.getBootcamps = async (req, res, next) => {
@@ -17,13 +18,15 @@ exports.getBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findById(req.params.id);
     if (!bootcamp) {
-      return res.status(400).json({
-        success: false
-      });
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 400)
+      );
     }
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    next(err);
+    next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 400)
+    );
   }
 };
 
